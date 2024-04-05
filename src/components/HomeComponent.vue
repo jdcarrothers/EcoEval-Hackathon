94802f7b012285d35dd41639c5e28b15ff80b5e9
Joshua Carrothers <jdcarrothers@icloud.com>
2024-04-05 23:11:40 +0100

    pre hackathon

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h1>Current Tab URL</h1>
+    <p>{{ currentUrl }}</p>
+</div>
+</template>
+
+<script>
+/* global chrome */
+export default {
+name: 'HomeComponent',
+data() {
+    return {
+    currentUrl: ''
+    }
+},
+mounted() {
+    this.getCurrentTabUrl();
+},
+methods: {
+    getCurrentTabUrl() {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        if (tabs.length > 0) {
+        this.currentUrl = tabs[0].url;
+        }
+        console.log(this.currentUrl);
+    });
+    }
+}
+}
+</script>
+
+<style scoped>
+/* Your styles here */
+</style>
